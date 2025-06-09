f055020b46cf57e8222fd81135047432a0d5f920
IanRMarques <99884736+IanRMarques@users.noreply.github.com>
2025-06-09 11:47:31 -0300

    Delete server directory

--- server/src/middlewares/loggerMiddleware.ts
+++
@@ -1,8 +0,0 @@
-// src/middlewares/loggerMiddleware.ts
-
-import { Request, Response, NextFunction } from 'express';
-
-export function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(`${req.method} ${req.url}`);  // Log da requisição com método HTTP e URL
-  next();  // Passa a requisição para o próximo middleware
-}
