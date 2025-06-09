124fcf1a8ee4c90f3836ecc4d31e8bbe6fb29508
IanRMarques <99884736+IanRMarques@users.noreply.github.com>
2025-06-09 11:44:41 -0300

    Delete client directory

--- client/src/api/axios.ts
+++
@@ -1,8 +0,0 @@
-// src/api/axios.ts
-
-import axios from 'axios'
-
-// Configuração direta do axios com a URL base
-export const api = axios.create({
-  baseURL: 'http://localhost:3333' // Defina a URL diretamente sem usar import.meta
-})
